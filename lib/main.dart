5268d7208503080a39f87cd84b6d1fdbb6c89941
CometMotion <orozbaevelmar@gmail.com>
2023-12-12 21:58:34 +0600

    main Folder chandeg

--- lib/main.dart
+++ lib/main.dart
@@ -56,6 +56,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  final num = 13;
 
   void _incrementCounter() {
     setState(() {
