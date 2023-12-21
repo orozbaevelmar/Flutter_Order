9585cf9e76a97ce5e5b79c710914a82419e94135
CometMotion <orozbaevelmar@gmail.com>
2023-12-21 11:07:45 +0600

    sixth commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,10 +8,20 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        //changed
+        appBar: AppBar(
+          actions: [
+            Icon(Icons.abc),
+            Container(
+              color: Colors.red,
+            )
+          ],
+        ),
         body: SafeArea(
           child: SingleChildScrollView(
             scrollDirection: Axis.vertical,
